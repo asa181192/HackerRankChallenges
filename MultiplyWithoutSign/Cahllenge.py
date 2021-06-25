a0b3060f5bf4c3bdee5e28c74f6900f9b3c895ed
Alfredo Santiago <asa181192@hotmail.es>
2021-06-24 19:22:34 -0500

    Calculate without the operator *

---
+++ MultiplyWithoutSign/Cahllenge.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Need to calculate a value without use the operator * 
+#
+
+
+def Multiply(a, b):
+    i = 0
+    result = 0
+    
+    while i < abs(b):
+
+        result = result + a
+        i += 1
+    
+    if b < 0 and a < 0:
+        return abs (result)
+    elif b < 0: 
+        return - result   
+    else:
+        return result
+
+
+if __name__ == '__main__':
+
+    a = 2
+    b = 2
+    number = Multiply(a, b)
+    print(number)

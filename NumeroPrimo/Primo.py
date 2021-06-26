d899da25bccda180b5ea85cd728fcd915846a3e6
Alfredo Santiago <asa181192@hotmail.es>
2021-06-25 20:09:30 -0500

    change to recursive function

--- NumeroPrimo/Primo.py
+++ NumeroPrimo/Primo.py
@@ -1,27 +1,30 @@
-def numeroPrimo(n):
-
-    esPrimo = 0
-
-    if n == 2:
+def numeroPrimo(n, i):    
+    if n == i:
         return 1
-    
-    #primo 
-    if n % 2 != 0:
-        if n % 3 != 0: 
-            esPrimo = 1
+    if n == 1 :
+        return 0
+    elif n % i == 0:
+        return 0
+    else:
+        return numeroPrimo(n, i + 1)
 
-    return esPrimo
-
+def esPrimo(n):
+    return numeroPrimo(n,2)
 # LUIS SIUL
 
 #  OSO , oxxxo
 
+
 if __name__ == '__main__':
-    
 
-    result = numeroPrimo(2027)
+      i = 1
+      while i <= 200:
+          result = esPrimo(i)
 
-    if result:
-        print("Numero es primo")
-    else:
-        print("numero no es primo")
+          if result:
+             print("Numero es primo " + str(i))
+          else:
+             print("numero no es primo " + str(i))
+
+          i += 1
+  
